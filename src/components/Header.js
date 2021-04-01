29e63aeaa17594803c2dbcd2c0c60a6579eae732
dagonib <davidgonzalezibanez@gmail.com>
2021-04-01 12:17:59 +0200

    Header Content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Row } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 
 import '../styles/header.styles.scss'
 
 const Header = () => {
     return (
-        <Row className='header d-flex align-items-center mx-0'>
-            <h1>Easy start with Vlava</h1>
-            <h2>We are team of talented designers making websites with Bootstrap</h2>
-            <a href="#about" className="btn-get-started scrollto">Get Started</a>
+        <Row className='header d-flex align-items-end mx-0'>
+            <Container className='header-container text-center text-white mb-5'>
+                <h1>La vida en rojo</h1>
+                <h2>Asociación de Cultura Menstrual</h2>
+                <p className='px-0 px-lg-5'>Es revelador y revolucionario conocer tu cuerpo, tu menstruación, tus ciclos… Tiene una capacidad de generar cambios individuales y colectivos muy potentes</p>
+            </Container>
         </Row>
     )
 }
