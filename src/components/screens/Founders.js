6b32e924901afda41013d66e4981b3cf3996fc74
dagonib <davidgonzalezibanez@gmail.com>
2021-04-06 11:30:29 +0200

    HomeScreen Purpose

---
+++ src/components/screens/Founders.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Row, Col, Image, Container } from 'react-bootstrap'
+
+const Founders = () => {
+    return (
+        <Container className='pb-5'>
+            <Row className='d-flex justify-content-center mb-4 m-0'>
+                <h1 className='m-0'>Fundadoras</h1>
+            </Row>
+            <Row className='m-0'>
+                cards
+            </Row>
+        </Container>
+    )
+}
+
+export default Founders
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
 import Header from '../components/Header'
-
+import Purpose from '../components/screens/Purpose'
+import Founders from '../components/screens/Founders'
 
 const HomeScreen = () => {
     return (
         <> 
             <Header />
-            <Container>
-                <p>Incicio</p>
-            </Container>
+            <Purpose />
+            <Founders />
         </>
     )
 }
