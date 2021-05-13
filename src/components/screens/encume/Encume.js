0c5b68a6a1cf4b1c773a47481987525a8d1fb3e8
dagonib <davidgonzalezibanez@gmail.com>
2021-05-13 19:10:32 +0200

    modifications 12.05.1

--- src/components/screens/encume/Encume.js
+++ src/components/screens/encume/Encume.js
@@ -6,7 +6,6 @@ import useWindowSize from '../../../utilities/useWindowSize'
 // Assets
 import img_encume_sm from '../../../assets/img/encume-sm.png'
 import img_encume_md from '../../../assets/img/encume-md.png'
-import img_encume_lg from '../../../assets/img/encume-lg.png'
 import logo_encume from '../../../assets/img/logos/logo-blanco-global.png'
 
 // Styles
@@ -16,44 +15,34 @@ const Encume = () => {
     const { width }  = useWindowSize()
 
     return (
-        <Row className='encume d-flex flex-column flex-md-row align-items-stretch mx-0'>
-            <Col xs={{ order: 'last' }} md={{ order: 'first' }} className='d-flex align-items-center py-5 px-0 px-sm-2 px-md-4 bg-secondary'>
-                <Container>                   
-                    <h2 className='title-section m-0 text-center text-white mb-3 m-0'>Evento anual</h2>
-                    <p className='encume-text normal-text px-0 m-0'>Los Encuentros de Cultura Menstrual (EnCuMe) son un evento anual autogestionado donde buscamos recoger la necesidad individual y colectiva de compartir investigación, lenguaje, vivencias, y debatir sobre las políticas públicas, sanitarias y educativas que sobre el ciclo menstrual se aplican en el Estado español. En estos encuentros proponemos un espacio compartido para reflexionar sobre los diversos temas socioculturales que condicionan la vida de las mujeres y personas menstruantes.</p>
-                    <p className='encume-text normal-text px-0 m-0'>En cada encuentro sentamos las bases para una definición de una nueva Cultura menstrual y una educación menstrual inclusiva, integral, sostenible y feminista.</p>
+        <Row className='encume d-flex flex-column-reverse flex-md-row bg-secondary px-0 mx-0 mb-5 p-md-5'>
+            <Col className='textbox d-flex align-self-center px-4 pt-3 pb-5 pl-md-0 pr-md-4 py-md-0 bg-secondary'>
+                <Container className='p-0'>                   
+                    <h2 className='title-section text-center text-white mb-3 m-0'>Evento anual</h2>
+                    <p className='normal-text text-white text-justify px-0 m-0'>Los Encuentros de Cultura Menstrual (EnCuMe) son un evento anual autogestionado donde buscamos recoger la necesidad individual y colectiva de compartir investigación, lenguaje, vivencias, y debatir sobre las políticas públicas, sanitarias y educativas que sobre el ciclo menstrual se aplican en el Estado español. En estos encuentros proponemos un espacio compartido para reflexionar sobre los diversos temas socioculturales que condicionan la vida de las mujeres y personas menstruantes.</p>
+                    <p className='normal-text text-white text-justify px-0 m-0'>En cada encuentro sentamos las bases para una definición de una nueva Cultura menstrual y una educación menstrual inclusiva, integral, sostenible y feminista.</p>
                     <Button 
                         href='http://www.encuentroculturamenstrual.es/'
                         target='_blank'
                         className='encume_button normal-text align-self-center rounded-0 mt-3'>EnCuMe 2021</Button>
                 </Container>
             </Col>
-            <Col xs={{ order: 'first' }} md={{ order: 'last' }} className='d-flex justify-content-center p-0'>
-                <Container className='encume-backgroundtext d-flex flex-column justify-content-between align-content-between'>
+            <Col className='imgbox d-flex flex-column-reverse flex-md-column align-self-center p-0 pl-md-4'>
                     <Image 
-                        className='align-self-center mt-5'
+                        className='encume_logo align-self-center mt-3 mt-md-0 mb-md-3'
                         alt='founder-1'
                         src={logo_encume}
-                        width='50%'
                     />
-                    <p className='normal-text px-0 px-sm-3 px-md-4 px-lg-5 mb-5 m-0'></p>
-                </Container>
-                    { width < 767 ? (
+                    { width > 767 ? (
                         <Image
-                            className='encume-image'
-                            src={img_encume_sm}
-                        />
-                    ) : ( width < 974 ? (
-                        <Image
-                            className='encume-image'
                             src={img_encume_md}
                         />
                     ) : (
-                        <Image
-                            className='encume-image'
-                            src={img_encume_lg}
-                        />
-                    ) )}
+                            <Image
+                                src={img_encume_sm}
+                            />
+                    )}
+                    
             </Col>
         </Row>
     )
